07c0145557f2965b2d7c00d58037986b7b09593b
liqigku <132674625+liqigku@users.noreply.github.com>
2023-07-15 12:22:58 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write('Hello world!')
+
+import streamlit as st
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
